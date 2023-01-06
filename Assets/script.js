939c6a04403cf1ecad664bdab87d23b312cba54d
TrentLe <raeburn.trent6@gmail.com>
2023-01-06 03:54:53 -0500

    Added code

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,44 @@
-var timerCount = 90;
+// Global variables
+var timerCount = 15;
+var startButton = document.getElementById('begin')
+var timerElement = document.getElementById('time')
 
+// Quiz container
 function startQuiz() {
-
+    
+// Function holding the quiz questions
     function quizQuestions() {
+        var myQuestions = [
 
+        {
+            question : 'Who is the first Hokage ?',
+            answers : {
+                a: 'Jiraya',
+                b: 'Hashirama',
+                c: 'Sasuke'
+            },
+            rightAnswer : 'b'
+        },
+
+        {
+            question : 'What is the sqrt of pi ?',
+            answers : {
+                a: '1.72',
+                b: 'huh ?',
+                c: '76'
+            },
+            rightAnswer : 'a'
+        }
+        ]
 
 
     }
-    
+// Function showing the questions to the user     
+    function userSight(){
+
+    }
+
+// Functions to show the results of the quiz
     function results() {
 
     }
@@ -16,13 +47,18 @@ function startQuiz() {
 function startTime() {
     timer = setInterval(function(){
         timerCount--;
-        timerElement.textContent = 'You have' + timerCount + 'seconds left';
+        timerElement.textContent = 'You have ' + timerCount + ' seconds left';
 
     },1000)
 }
-
+quizQuestions();
 startTime();
 
-submitButton
 
+
+}
+
+// Button to start the quiz
+startButton.onclick = function(){
+    startQuiz();
 }
